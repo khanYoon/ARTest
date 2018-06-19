89a80e0279763d0c9e0d69a34296a84041657c34
khanYoon <kh.yoon@maxted.kr>
2018-06-19 15:53:22 +0900

    tets
    stets

--- arModel/src/ar.js
+++ arModel/src/ar.js
@@ -9,49 +9,92 @@ AFRAME.registerComponent('ar', {
   },
   dependencies: ['three-ar', 'mozilla-xr-ar', 'ar-planes', 'ar-anchors'],
   getSource: function () {
-    var whichar;
-    if (!this.source) {
-      whichar = this.el.sceneEl.components['three-ar'];
-      if (whichar && whichar.arDisplay) {
-        this.source = whichar.arDisplay;
+    try {
+      var whichar;
+      if (!this.source) {
+        whichar = this.el.sceneEl.components['three-ar'];
+        if (whichar && whichar.arDisplay) {
+          this.source = whichar.arDisplay;
+        }
       }
-    }
-    if (!this.source) {
-      whichar = this.el.sceneEl.components['mozilla-xr-ar'];
-      if (whichar && whichar.arDisplay) {
-        this.source = whichar;
+      if (!this.source) {
+        whichar = this.el.sceneEl.components['mozilla-xr-ar'];
+        if (whichar && whichar.arDisplay) {
+          this.source = whichar;
+        }
       }
+      return this.source;
+    } catch (e) {
+alert(e);
+    } finally {
+
     }
-    return this.source;
+
   },
   getPlanes: function () {
-    return this.source ? this.source.getPlanes() : undefined;
-  },
-  getAnchors: function () {
-    return this.source ? this.source.getAnchors() : undefined;
-  },
-  addImage: function (name, url, physicalWidth) {
-    return this.source.addImage(name, url, physicalWidth);
-  },
-  removeImage: function (name) {
-    return this.source.removeImage(name);
-  },
-  init: function () {
-    var options = {
-      takeOverCamera: this.data.takeOverCamera,
-      cameraUserHeight: this.data.cameraUserHeight,
-      worldSensing: this.data.worldSensing
-    };
+    try {
+      return this.source ? this.source.getPlanes() : undefined;
+    } catch (e) {
+alert(e);
+    } finally {
 
-    this.el.setAttribute('three-ar', options);
-    this.el.setAttribute('mozilla-xr-ar', options);
-
-    if (this.data.hideUI) {
-      this.el.sceneEl.setAttribute('vr-mode-ui', {enabled: false});
     }
 
-    // Ensure passthrough is visible, make sure A-Frame styles don't interfere.
-    document.head.insertAdjacentHTML('beforeend', 
-      '<style>html,body {background-color: transparent !important;}</style>');
+  },
+  getAnchors: function () {
+    try {
+      return this.source ? this.source.getAnchors() : undefined;
+    } catch (e) {
+alert(e);
+    } finally {
+
+    }
+
+  },
+  addImage: function (name, url, physicalWidth) {
+
+    try {
+      return this.source.addImage(name, url, physicalWidth);
+    } catch (e) {
+alert(e);
+    } finally {
+
+    }
+  },
+  removeImage: function (name) {
+
+    try {
+      return this.source.removeImage(name);
+    } catch (e) {
+alert(e);
+    } finally {
+
+    }
+  },
+  init: function () {
+    alert("load");
+    try {
+      var options = {
+        takeOverCamera: this.data.takeOverCamera,
+        cameraUserHeight: this.data.cameraUserHeight,
+        worldSensing: this.data.worldSensing
+      };
+
+      this.el.setAttribute('three-ar', options);
+      this.el.setAttribute('mozilla-xr-ar', options);
+
+      if (this.data.hideUI) {
+        this.el.sceneEl.setAttribute('vr-mode-ui', {enabled: false});
+      }
+
+      // Ensure passthrough is visible, make sure A-Frame styles don't interfere.
+      document.head.insertAdjacentHTML('beforeend',
+        '<style>html,body {background-color: transparent !important;}</style>');
+    } catch (e) {
+alert(e);
+    } finally {
+
+    }
+
   }
 });
