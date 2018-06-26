4c00aa50fe4b0f41706705173f74fcda94381ee8
khanYoon <kh.yoon@maxted.kr>
2018-06-26 17:23:40 +0900

    test
    test

--- monopoly/js/utils2.js
+++ monopoly/js/utils2.js
@@ -291,10 +291,10 @@ utils = {
 						// x축으로 움직일때
 						if(typeof movePosition.x === "number"){
 							if(sPosition.x > ePosition.x){
-								obj.object3D.position.x += (movePosition.x * -1) / 4;
+								obj.object3D.position.x += ((movePosition.x * -1) / 4) / 10;
 								alert('if obj.object3D.position.x: ' + obj.object3D.position.x);
 							}else{
-								obj.object3D.position.x += movePosition.x / 4;
+								obj.object3D.position.x += (movePosition.x / 4) / 10;
 								alert('else obj.object3D.position.x: ' + obj.object3D.position.x);
 							}
 						}
@@ -309,10 +309,10 @@ utils = {
 						// z축으로 움직일때
 						if(typeof movePosition.z === "number"){
 							if(sPosition.z > ePosition.z){
-								obj.object3D.position.z += (movePosition.z * -1) / 4;
+								obj.object3D.position.z += ((movePosition.z * -1) / 4) / 10;
 								alert('if obj.object3D.position.x: ' + obj.object3D.position.z);
 							}else{
-								obj.object3D.position.z += movePosition.z / 4;
+								obj.object3D.position.z += (movePosition.z / 4) / 10;
 								alert('else obj.object3D.position.x: ' + obj.object3D.position.z);
 							}
 						}
