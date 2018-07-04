e700e7cbfb196b6c9c4b436b48f1d6a58f2fad09
khanYoon <kh.yoon@maxted.kr>
2018-07-04 13:39:07 +0900

    test
    test

--- arDetection/js/showImage-marker.js
+++ arDetection/js/showImage-marker.js
@@ -1,5 +1,5 @@
 // ARKit image anchor orientations need adjustment to apply to A-Frame plane-based entities.
-var imageAnchorToPlaneQuat = new THREE.Quaternion();          
+var imageAnchorToPlaneQuat = new THREE.Quaternion();
 imageAnchorToPlaneQuat.setFromAxisAngle(new THREE.Vector3(1,0,0), THREE.Math.DEG2RAD * -90);
 
 AFRAME.registerComponent('show-at-image-marker', {
@@ -19,13 +19,11 @@ AFRAME.registerComponent('show-at-image-marker', {
     if (!this.source) {
       this.source = document.querySelector('[ar]').components.ar.getSource();
     }
-    
+
     if (!this.source) { return; }
-    
+
     if (!this.added) {
-      this.source.addImage(this.data.name, 
-                         this.data.src,
-                         this.data.physicalWidth);
+      this.source.addImage(this.data.name, this.data.src, this.data.physicalWidth);
       this.added = true;
       return;
     }
@@ -38,17 +36,17 @@ AFRAME.registerComponent('show-at-image-marker', {
         if (anchors[i].name === this.data.name) {
           var mat = new THREE.Matrix4().fromArray(anchors[i].modelMatrix);
           mat.decompose(this.el.object3D.position, this.el.object3D.quaternion, this.el.object3D.scale);
-          if (this.data.vertical) { 
+          if (this.data.vertical) {
             this.el.object3D.quaternion.multiply(imageAnchorToPlaneQuat);
           }
-          
+
           if (!this.el.getAttribute('visible')) {
-            this.el.setAttribute('visible', true);                
+            this.el.setAttribute('visible', true);
             this.el.emit('imageanchor', {anchor: anchors[i]});
           } else {
             this.el.emit('imageanchorupdate', {anchor: anchors[i]});
           }
-          
+
           // Remove the image since we've detected it.
           if (!this.removed) {
             this.removed = true;
@@ -57,5 +55,5 @@ AFRAME.registerComponent('show-at-image-marker', {
         }
       }
     }
-  }        
+  }
 });
