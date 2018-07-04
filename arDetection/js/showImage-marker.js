2fff5aa7764dd148891c80463c0951d3c59b0971
khanYoon <kh.yoon@maxted.kr>
2018-07-04 14:04:07 +0900

    test
    test

--- arDetection/js/showImage-marker.js
+++ arDetection/js/showImage-marker.js
@@ -23,6 +23,7 @@ AFRAME.registerComponent('show-at-image-marker', {
     if (!this.source) { return; }
 
     if (!this.added) {
+      alert("여기는 들어옴!")
       this.source.addImage(this.data.name, this.data.src, this.data.physicalWidth);
       this.added = true;
       return;
